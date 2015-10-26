08c725cdff7a69ee88033fb64147c234ecedf689
Amit <amitshob@gmail.com>
2015-10-25 21:44:35 -0700

    HABTM relationship working

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,10 +3,13 @@ class TagsController < ApplicationController
     @post = Post.find(params[:post_id])
     @tag = @post.tags.new
   end
+
   def create
     @post = Post.find(params[:post_id])
     @tag = @post.tags.new(tag_params)
-    if @tag.save
+    # if @tag.save
+    if @tag.valid?
+     @post.tags << @tag
       redirect_to post_path(@post)
     else
       render :new
@@ -28,8 +31,13 @@ class TagsController < ApplicationController
   end
 
   def show
+    # @post = Post.find(params[:post_id])
+    # # @tag = @post.tags.new(tag_params)
+    # @tags = Tag.all
+    @tag = Tag.find(params[:id])
+   render :show
   end
-  
+
   def destroy
     @post = Post.find(params[:post_id])
     @tag = @post.tags.new(tag_params)
